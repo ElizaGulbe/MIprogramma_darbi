f2f5d117b9515d001c62fc8e902db5663477ab2e
ElizaGulbe <eliza.gulbe96@gmail.com>
2021-09-27 18:09:57 +0300

    Functions + apperance

--- Majsadarbi/majasdarbs1.cpp
+++ Majsadarbi/majasdarbs1.cpp
@@ -2,75 +2,80 @@
 #include <string>
 using namespace std;
 
-string isOdd(string usrInpt){
-    if(usrInpt.size()%2 == 1){
-        return "nepara";
+void isOdd(string userInput){
+   string isOdd; 
+    if(userInput.size() % 2 == 1){
+        isOdd = "nepara";
     } else{
-        return "para";
+        isOdd = "para";
     }
+    cout << "'" << userInput << "' teksta garums ir  " << isOdd << "  skaitlis" << endl; 
+}
+void displayPossibilites(){
+    cout << "1: Ievadit jaunu tekstu" << endl; 
+    cout << "2: Pasaka vai ievadita teksta garums ir para vai nepara skaitlis " << endl; 
+    cout << "3. Izvada summu no 1..n (kur n = teksta garums)" << endl; 
+    cout << "4. Atrod faktoriali n (kur n = teksta garums" << endl; 
+    cout << "5. Izvada virkni no otra gala (reversa)" << endl; 
+    cout << "6. Beigt darbibu" << endl; 
+}
+void insertNewTxt(string& usinptVal){
+    cout << "Ievadiet jaunu tekstu" << endl;
+    cin >> usinptVal; 
+}
+void arithmeticProgression(string userInputValTxt){
+    int arithmeticProgressionofN; 
+    arithmeticProgressionofN = (1+userInputValTxt.size())*userInputValTxt.size()/2;
+    cout << "Teksta garuma aritmetiskas progresijas summa ir " << arithmeticProgressionofN << endl;
+}
+void computeFactorial(string userInputValueTxt){ 
+    int factorial; 
+    factorial = 1;
+    for(int i = 1; i <= userInputValueTxt.size() ;i++){
+        factorial = i * factorial; 
+    }
+    cout << "'" << userInputValueTxt << "' teksta garums faktorialis ir " << factorial  << endl;
 }
 
+void reverseInput(string userInputValueTxt){
+    string stringReverse = ""; 
+    for(char i : userInputValueTxt){
+            stringReverse = i + stringReverse;
+        }
+    cout << "apgreizta vertiba tekstam" << " '" << userInputValueTxt << "' ir  '" << stringReverse << "'" << endl; 
+}
 int main(){
-cout << "1: Ievadit jaunu tekstu" << endl; 
-cout << "2: Pasaka vai ievadita teksta garums ir para vai nepara skaitlis " << endl; 
-cout << "3. Izvada summu no 1..n (kur n = teksta garums)" << endl; 
-cout << "4. Atrod faktoriali n (kur n = teksta garums" << endl; 
-cout << "5. Izvada virkni no otra gala (reversa)" << endl; 
-cout << "6. Beigt darbibu" << endl; 
-
-string userInputValueTxt; 
-userInputValueTxt = "Teksts ir loti loti interesants";
+displayPossibilites();
+string userInputValueTxt = "Teksts ir loti loti interesants";
 int commandNumber; 
-
 cout << "Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl; 
 cin >> commandNumber; 
-
-string stringReverse; 
-stringReverse = "";
-
 while(1){
     switch (commandNumber){
     case 1:
-        cout << "Ievadiet jaunu tekstu" << endl;
-        cin >> userInputValueTxt; 
+        insertNewTxt(userInputValueTxt);
         break;
     case 2:
-        cout << "'" << userInputValueTxt << "' teksta garums ir  " << isOdd(userInputValueTxt) << "  skaitlis" << endl; 
+        isOdd(userInputValueTxt);
         break;
     case 3:
-        int arithmeticProgressionofN; 
-        arithmeticProgressionofN = (1+userInputValueTxt.size())*userInputValueTxt.size()/2;
-        cout << "Teksta garuma aritmetiskas progresijas summa ir " << arithmeticProgressionofN << endl;
+        arithmeticProgression(userInputValueTxt);
         break;
-    case 4: 
-        int factorial; 
-        factorial = 1;
-        for(int i = 1; i <= userInputValueTxt.size() ;i++){
-            factorial = i * factorial; 
-        }
-        cout << "'" << userInputValueTxt << "' teksta garums faktorialis ir " << factorial  << endl; 
+    case 4:  
+        computeFactorial(userInputValueTxt);
         break;
     case 5: 
-        
-        for(char i : userInputValueTxt){
-            stringReverse = i + stringReverse;
-        }
-        cout << "apgreizta vertiba tekstam" << " '" << userInputValueTxt << "' ir  '" << stringReverse << "'" << endl;
-        stringReverse = "";
+        reverseInput(userInputValueTxt);
         break;
     case 6:
         cout << "Programmas darbiba izbeigta" << endl; 
         return 0; 
-
     default:
-        cout << "Nepareiza vertiba. Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl; 
+        cout << "Nepareiza vertiba.Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl; 
         break;
     }
-
+    displayPossibilites();
     cin >> commandNumber; 
-    
-
 }
-
-return 0;
+    return 0;
 }
