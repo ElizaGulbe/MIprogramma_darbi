66096dcd330741f5bd5ca00c28fdb4023790f218
ElizaGulbe <eliza.gulbe96@gmail.com>
2021-10-11 19:00:51 +0300

    Veikti uzlabojumi

--- Majsadarbi/Majasdarbs2.cpp
+++ Majsadarbi/Majasdarbs2.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <limits>
 #include <stack>
 #include <vector>
 #include <cstdlib>
 using namespace std;
 
-
-void createAndDisplayQueue(const vector<int> &initialValue){
+void createAndDisplayQueue(const vector<int> &initialValue) {
     queue<int> myqueue;
-    for(int i = 0 ; i < initialValue.size() ; i++) {
-        myqueue.push(initialValue[i]); // pievieno vectoru stack
+    for (int i : initialValue){
+        myqueue.push(i); // pievieno vectoru stack
     }
-    cout << "queue dequeue seciba" << endl; 
-    for (int i = 0 ; i < initialValue.size() ; i++) {
-        cout << myqueue.front() << " " ;
+    cout << "queue dequeue seciba" << endl;
+    for (int i : initialValue)
+    {
+        cout << myqueue.front() << " ";
         myqueue.pop();
     }
     cout << endl;
 }
-void displayOpportunities(){
+void displayOpportunities() {
     cout << "Iespejamas darbibas ir: " << endl;
     cout << "1: Ievadit skaitlus" << endl;
     cout << "2: Rinda" << endl;
@@ -30,108 +31,111 @@ void displayOpportunities(){
     cout << "ludzu ievadiet skaitli" << endl;
 }
 
-void createAndDisplayStack(const vector<int> &initialValue){
-stack<int> data;
-    for(int i = 0 ; i < initialValue.size() ; i++) {
-        data.push(initialValue[i]); // pievieno vectoru stack
+void createAndDisplayStack(const vector<int> &initialValue) {
+    stack<int> data;
+    for (int i : initialValue) {
+        data.push(i); // pievieno vectoru stack
     }
-    cout << "Stack pop seciba" << endl; 
-    for (int i = 0 ; i < initialValue.size() ; i++) {
-     cout << data.top() << " ";
-     data.pop();
+    cout << "Stack pop seciba" << endl;
+    for (int i = 0; i < initialValue.size(); i++) {
+        cout << data.top() << " ";
+        data.pop();
     }
     cout << endl;
 }
 
-void createAndDisplayDoubleStack(const vector<int> &initialValue){
- stack<int> stack1;
- stack<int> stack2;
- for(int i = 0 ; i < initialValue.size() ; i++) {
-     stack1.push(initialValue[i]); // pievieno vectoru pirmajam stack
- }
- cout << "Double stack pop seciba" << endl; 
-for (int i = 0 ; i < initialValue.size() ; i++) {
-     stack2.push(stack1.top());  // parliek stack1 - > stack 2 ; 
-     stack1.pop();
- }
-for (int i = 0 ; i < initialValue.size() ; i++) {
-     cout << stack2.top() << " ";
-     stack2.pop();   // iznem no  stack2 value un izprinte vertibu
+void createAndDisplayDoubleStack(const vector<int> &initialValue) {
+    stack<int> stack1;
+    stack<int> stack2;
+    for (int i : initialValue) {
+        stack1.push(i); // pievieno vectoru pirmajam stack
     }
-cout << endl;
-
+    cout << "Double stack pop seciba" << endl;
+    for (int i = 0; i < initialValue.size(); i++) {
+        stack2.push(stack1.top()); // parliek stack1 - > stack 2 ;
+        stack1.pop();
+    }
+    for (int i = 0; i < initialValue.size(); i++) {
+        cout << stack2.top() << " ";
+        stack2.pop(); // iznem no  stack2 value un izprinte vertibu
+    }
+    cout << endl;
 }
-
 
 void displayList(vector<int> &initialValue) {
     string retVal = "[";
-    for(int i = 0 ; i < initialValue.size(); i++) {
+    for (int i = 0; i < initialValue.size(); i++) {
         if (i == initialValue.size() - 1) {
             retVal = retVal + to_string(initialValue[i]) + "]";
-        } else {
-            retVal = retVal + to_string(initialValue[i]) + ",";  
         }
-        
+        else {
+            retVal = retVal + to_string(initialValue[i]) + ",";
+        }
     }
-    
-    cout << retVal << endl;  
+
+    cout << retVal << endl;
+}
+void insertNewElements(vector<int> &initialValue) {
+    cout << "ievadiet 1 skaitli" << endl;
+    int number; // ievada pa vienam skaitlim lidz lietotajs vairs nevelas ievietot skaitli
+    char response = 'y';
+    while (response == 'y') {
+        cout << "ievadiet 1 skaitli" << endl;
+        cin >> number;
+        while (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ir ievadita nepareiza vertiba. Ludzu ievadiet veselu skaitli" << endl;
+            cin >> number;
+        }
+        initialValue.push_back(number);
+        cout << "vai velaties ievadit vel vienu skaitli? y - Ja , n - ne" << endl;
+        cin >> response;
+    }
 }
 
 int switchcase(const int &inputData, vector<int> &initialValue) {
     switch (inputData) {
-        case 1:
-            cout << "ievadiet 1 skaitli" << endl; 
-            int number;     // ievada pa vienam skaitlim lidz lietotajs vairs nevelas ievietot skaitli 
-            cin >> number; 
-            initialValue.push_back(number); 
-            char response; 
-            cout << "vai velaties ievadit vel vienu skaitli? y - Ja , n - ne" << endl;
-            cin >> response;
-            while(response == 'y') {
-                cout << "ievadiet 1 skaitli" << endl; 
-                cin >> number; 
-                initialValue.push_back(number); 
-                cout << "vai velaties ievadit vel vienu skaitli? y - Ja , n - ne" << endl;
-                cin >> response;
-            }
-            return 1;
-            break;
-        case 2:
-            createAndDisplayQueue(initialValue);
-            return 1;
-            break;
-        case 3:
-            createAndDisplayStack(initialValue);
-            return 1;
-            break;
-        case 4: 
-            createAndDisplayDoubleStack(initialValue);
-            return 1;
-            break;
-        case 5:
-            displayList(initialValue);
-            return 1;
-            break;
-        case 6:
-            cout << "Programmas darbiba izbeigta" << endl; 
-            return 0; 
-        default:
-            cout << "Nepareiza vertiba.Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl;
-            return 0;  
-            break;
-        }
-
+    case 1:
+        insertNewElements(initialValue);
+        return 1;
+    case 2:
+        createAndDisplayQueue(initialValue);
+        return 1;
+    case 3:
+        createAndDisplayStack(initialValue);
+        return 1;
+    case 4:
+        createAndDisplayDoubleStack(initialValue);
+        return 1;
+    case 5:
+        displayList(initialValue);
+        return 1;
+    case 6:
+        cout << "Programmas darbiba izbeigta" << endl;
+        return 0;
+    default:
+        cout << "Nepareiza vertiba.Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl;
+        return 1;
+    }
 }
-int main(){
+int main() {
     vector<int> initialValue;
-    for (int i = 4 ; i < 9 ; i++ ) {
+    for (int i = 4; i < 9; i++) {
         initialValue.push_back(i);
-    } 
-     while (1) {
+    }
+    while (1) {
         displayOpportunities();
         int inputData;
         cin >> inputData;
-         if(switchcase(inputData,initialValue) == 0) break; 
+        while (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ir ievadita nepareiza vertiba.Ludzu ievadiet vertibu no 1(ieskaitot) lidz 6(ieskaitot)" << endl;
+            cin >> inputData;
+        }
+        if (switchcase(inputData, initialValue) == 0)
+            break;
     }
     return 0;
 }
